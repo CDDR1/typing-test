55eb2ec3dafe9ea09c61374254ea8a73523c784c
Carlos Duque <carlos.d.duque.r@gmail.com>
2024-02-01 21:41:05 -0500

    Adding correct and incorrect text color functionality.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+let intervalId = null;
+let currentLetterIndex = 0;
+const correctLetterIndices = new Set();
+const incorrectLetterIndices = new Set();
+
 const decrementTimer = (timer) => {
   const timerValue = parseInt(timer.textContent);
   if (timerValue === 1) {
@@ -6,9 +11,41 @@ const decrementTimer = (timer) => {
   timer.textContent = timerValue - 1;
 };
 
-let intervalId;
-
 const startTimer = () => {
   const timer = document.querySelector(".timer");
   intervalId = setInterval(() => decrementTimer(timer), 1000);
 };
+
+const checkKeydown = (event) => {
+  const key = event.key;
+  if (key.match(/[a-z]/i) && intervalId === null) {
+    startTimer();
+  }
+
+  const wordsWrapper = document.querySelector(".words-wrapper");
+  const text = wordsWrapper.innerText;
+  if (key === text[currentLetterIndex]) {
+    correctLetterIndices.add(currentLetterIndex);
+  } else {
+    incorrectLetterIndices.add(currentLetterIndex);
+  }
+  currentLetterIndex++;
+
+  const updatedText = text
+    .split("")
+    .map((char, index) => {
+      if (correctLetterIndices.has(index)) {
+        return `<span class="correct-key">${char}</span>`;
+      } else if (incorrectLetterIndices.has(index)) {
+        return `<span class="incorrect-key">${char}</span>`;
+      } else {
+        return char;
+      }
+    })
+    .join("");
+
+  wordsWrapper.innerHTML = updatedText;
+};
+
+const body = document.querySelector("body");
+body.addEventListener("keydown", (event) => checkKeydown(event));
